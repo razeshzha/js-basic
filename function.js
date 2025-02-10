f63b0cee27d1243c6102fcff0449b522c7b89d1a
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-10 09:29:05 +0545

    js

---
+++ function.js
@@ -0,0 +1,50 @@
+// function
+
+//functin functin_name ()
+//{
+     
+//}
+
+//function greet () {
+    //console.log("hello");
+
+//}
+//greet();
+
+//const greet = function (){
+    //console.log("hello");
+//}
+//greet();
+
+//const hello = "hdf"
+
+//greet();
+
+//const greet = function (name = "world"){
+   // console.log("hello" + name),
+   // console.log(`hello ${name}`);
+
+//}
+//greet("john");
+
+//const add = function (a,b){
+   // return a + b;
+//};
+
+
+//const add = (a,b) => a+ b;
+//const result = add (5,10)
+//console.log(result);
+
+//const test = (age) => (age >= 18 ? "is adult " : "is minor");
+//const result = test(16);
+//console.log(result);
+
+// iife
+(function (){
+    console.log("iife");
+
+})();
+
+
+
