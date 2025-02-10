8fb30d2be88daa554e8e8dc2daca748a0a1619aa
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-10 09:35:09 +0545

    js

---
+++ operator.js
@@ -0,0 +1,92 @@
+// operator => symbol which performs specific operation
+
+// arithmetic operators
+// assignment operators
+// comparison operators
+// logical operators
+
+// ? arithmetic operators
+
+// + ,-,*, /,%,**
+
+// console.log(2+3);
+// console.log(10-5);
+// console.log(5*5);
+// console.log   (10/2);
+// console.log(5**5);
+
+//console.log(11 % 2);
+
+// ? assignment operator
+// =,+=,-=,*=,/=,**=,%=
+
+// let userName = 'john';
+// console.log(userName);
+
+//let x = 5;
+//x = x + 2;         //x += 2;
+//console.log(x);
+
+//let y = 5;
+//y *= 2; //y = y*2;
+//console.log(y);
+
+//let num = 25;
+//num %= 2; //num = num %2;
+
+//console.log(num);
+
+// ? comparison
+// ? at least two entity
+// ? results in boolean (true/false)
+// ==,===,!=,!==,<,<=,>,>=
+
+// console.log(2 === 2);
+// console.log(5 != 3);
+
+// == this is weak comparison
+//  check for value equality
+// console.log(5 == "5");
+
+// ?
+// this is strict comparison
+// checks for value + checks for data types
+
+// console.log(5 === "5");
+// console.log(5+2);
+// console.log('N' !== "n");
+
+// ? logical operator
+
+// results in boolean (true /false)
+
+// ? or (&&)
+// ? or (||)
+// ? or (!)
+
+// ? or (||)
+// plus gate
+//  result is true => if any one input is true
+//  short circuit : if any one input is true, result is always true/ no need to check other input
+
+//let c1 = false;
+//let c2 = true;
+//let c3 = false;
+
+//console.log(c1 || c2 || c3);
+
+//let c1 = true;
+//let c2 = false;
+//let c3 = true;
+//console.log(c1 && c2 && c3);
+//  c2 is false => result is false
+//  no need to check c3
+
+// ? not (!);
+
+//let result = 2 >= 10; // false
+//console.log(result);
+
+// ? not (!);
+let result = 2 >= 10; // true
+console.log(!result);
