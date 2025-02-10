a842a3f5b90cb6917203b94f9ab52b71f97096ef
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-10 09:22:01 +0545

    data

---
+++ data_types.js
@@ -0,0 +1,47 @@
+// codeshare.io / MkKeVn;
+
+// data types
+//  type of data that variable can store
+
+// string
+// number
+// boolean
+// undefined
+// null
+// object
+// symbol
+// bigInt
+
+//? string
+//  "" (double quote)
+// '' (single quote)
+// ``(back=tick)
+// let x = `123`;
+// console.log(x, typeof x);
+
+//? number
+// ? NaN => Not a Number ("nepal"/25)
+// let num =25;
+// console.log(num, typeof num);
+
+// ? boolean
+// true or false
+// let isEven = false;
+// console.log(isEven, typeof isEven);
+
+// ? undefined
+// let sum;
+// console.log(sum, typeof sum);
+
+// ? null
+// let sum = null; // data type of null is object which is historic bug
+// console.log(sum, typeof sum);
+
+// object
+// key value pair
+const user = {
+  firstName: "Rohan",
+  lastName: "Bhandari",
+};
+
+console.log(user, typeof user);
