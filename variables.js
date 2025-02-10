cdcbc92730367428abb1cb884ca0e4e632ad8a63
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-10 09:37:56 +0545

    js

---
+++ variables.js
@@ -0,0 +1,109 @@
+// variables => rewriteable memory space
+// can be changed during program executtion
+
+// let x = 2;
+// x = 7;
+
+// console.log(x);
+
+// x = 9;
+// console.log(x);
+
+// const x = 2; //it is a constant
+// x = 5;
+// console.log(x);
+
+// ? variable naming convention
+// 1. variable name cannot be  reserved words such as let, const, while, for, etc...
+// ?bad example
+// let let =5
+// let const =5
+
+// ? good example
+//let FirstName = "Rohan";
+//const PI = 3.1415;
+
+// 2. variable name cannot start with numbers
+// ? bad example
+// let 1x =5
+// let 25abc = 9
+
+// ? good example
+//let num1 = 5;
+//let u2ser = "rohan";
+// allowed
+
+// * rule 3
+// 3. variable name cannot contain special characters such as
+// @, #, %, ^& etc...
+// except_ and $
+
+// ?not allowed
+// let abc# =5
+// let x^d =9
+
+//  ? allowed
+//let user$ = "abc";
+//let $ = 100;
+//let = 9;
+//let First_name = "Rohan";
+
+//  * rule 4
+// 4. variable name should be descriptive
+
+// bad example
+//let t = 40;
+// t is sum
+
+// good example
+//let temperature = 40;
+//let sum = 2 + 3;
+
+// * rule 5
+// 5. variable  name should be camel case
+// uppercase => FIRSTNAME
+// lowercase => firstname
+// camelCase => firstName, addNumbers, FindOddOrEven, dateOfBirth, permanentAdress
+
+// ?bad example
+// let firstname;
+// let oddOrEven;
+
+// ? good example
+// let firstName
+// let oddOrEven;
+
+// constant naming convention
+// ? same as variable naming convention
+// uni
+// universal constants are written in uppercase
+// const PI = Math.Pi;
+//const GRAVITY_ON_EARTH = 9.8;
+
+// const x = 5;
+//console.log(PI);
+//console.log(GRAVITY_ON_EARTH);
+//let age = 18;
+//let status = (age <= 18) ? "Adult" : "Minor";
+//console.log(status); // "Adult"
+//let table = "users";
+//let query = `SELECT * FROM ${table} WHERE age > 18`;
+////console.log(query);
+// Output: SELECT * FROM users WHERE age > 18
+//console.log(true && true);   // true
+//console.log(true && false);  // false
+//console.log(false && true);  // false
+//console.log(false && false); // false
+
+//let age = 20;
+//console.log(age > 18 && age < //30); // true (both conditions are true)
+//console.log(age < 30); // true  ( both conditions  are true)
+//console.log(age && age < 30); // 
+//  
+
+
+
+
+
+
+
