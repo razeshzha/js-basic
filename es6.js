08114ec095e2345d3379627ce7782afc28210be9
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-10 09:28:21 +0545

    js

---
+++ es6.js
@@ -0,0 +1,69 @@
+// es6
+// ....
+
+// concat, destructing ,rest,arr+object ,expand,speed
+
+// const arr1 =[1,2,3]
+// let arr2 = [3,4,5]
+
+//  const arr = arr2.concat()
+// arr2 = [...arr2,...arr]
+// console.log(arr2);
+
+//....rest
+
+
+//  function add(...numbers){
+
+//  }
+
+// map
+// var map = new Map()
+// console.log(map);
+
+// const person ={
+//     name:"john",
+//     age: 20,
+// }
+
+// var map = new Map()
+
+// map.set("name", "john");
+// map.set("age",10);
+// map.set(person,"Map with object key");
+//     console.log(map);
+
+// const person ={
+//     name:"john",
+//     age: 20,
+// }
+// map.set("name", "john");
+//  map.set("age",10);
+//  map.set(person,"Map with array");
+//      console.log(map);
+
+//map has
+//map.has
+
+//set
+
+//let set = new Set([1,2,3,4,4,3])
+// set.add('1')
+// set.add(1)
+// set.add(1)
+//console.log(set);
+
+
+
+// const array = [1,2,3,4,4,3]
+// const array2 =[...new Set(array2)]
+
+// console.log(array2);
+
+
+//
+// ''
+// ""
+// ``
+// $
+
