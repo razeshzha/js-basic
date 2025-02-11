5246b8e908db44fcf1d1b5fe88830c60113f5475
Rajesh-zha <Zharazesh89@gmail.com>
2025-02-11 19:51:27 +0545

    add:hacking1.js

---
+++ hacking1.js
@@ -0,0 +1,25 @@
+// 
+// let number = 34;
+// console.log(number);
+
+
+// arr = ["ram", "shyam","hari"];
+// console.log(arr);
+
+
+
+// let product="apple"
+
+// object = {
+//     product :"apple",
+//     name:"rajesh",
+//     quantiy: "5kg",
+//     address:"janakpur"
+
+    
+    
+   
+
+// }
+
+// console.log(typeof product.length);
